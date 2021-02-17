676d2617783970fd01265e34d269e2a7577180ac
JD-Jaramillo <jaramillo784@gmail.com>
2021-02-16 20:52:52 -0500

    Initial commit of ReadME, HTML, CSS and Javascript

---
+++ Assets/animation.js
@@ -0,0 +1,35 @@
+let timerEl = document.getElementById('#timer');
+let startBtn = document.getElementById('#button');
+let time = 60;
+
+
+
+function startQuiz() {
+    console.log("start Quiz");
+
+    // timer starts down from 60 seconds
+
+
+    // question appears and it has 4 options to choose from, 
+
+}
+
+
+
+
+
+// function for showing final resultss, user can save initials and score 
+
+function finalResults() {
+    console.log("Final Results");
+}
+
+
+
+
+
+
+
+startBtn.addEventListener("click", startQuiz);
+
+
